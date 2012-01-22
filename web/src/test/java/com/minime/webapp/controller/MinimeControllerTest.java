0b50d1483936feeac54840af362ba954df6846e7
Zabil CM <zabilcm@gmail.com>
2012-01-22 16:53:38 +0530

    Adedd controller tests framework and did the simple assertion in the url mapping.

---
+++ web/src/test/java/com/minime/webapp/controller/MinimeControllerTest.java
@@ -0,0 +1,32 @@
+package com.minime.webapp.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.server.RequestBuilder;
+import org.springframework.test.web.server.request.MockMvcRequestBuilders;
+import org.springframework.test.web.server.result.MockMvcResultMatchers;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.server.setup.MockMvcBuilders.standaloneSetup;
+
+public class MinimeControllerTest {
+
+    private MinimeController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        controller = new MinimeController();
+    }
+
+    @Test
+    public void testGreet() throws Exception {
+        standaloneSetup(controller).build().perform((get("/hello"))).andExpect(content().string(containsString("Hello Mini me!")));
+    }
+}
