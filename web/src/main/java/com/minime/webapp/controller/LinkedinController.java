816d371009731bb7533195127dc3c394149929ee
Jasdeep Madan <jasdeepm@gmail.com>
2012-01-31 22:17:31 +0530

    Adding controller for linkedin module

--- web/src/main/java/com/minime/webapp/controller/LinkedinController.java
+++ web/src/main/java/com/minime/webapp/controller/LinkedinController.java
@@ -1,21 +1,19 @@
 package com.minime.webapp.controller;
 
+import com.minime.LinkedIn;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.minime.Minime;
-
 @Controller
-@RequestMapping(value = "/hello")
-public class MinimeController {
+@RequestMapping(value = "/linkedin")
+public class LinkedinController {
 
     @RequestMapping(method = RequestMethod.GET)
     public
     @ResponseBody
-    Minime greet() {
-        return new Minime();
+    LinkedIn getLinked(){
+        return new LinkedIn();
     }
-
 }
